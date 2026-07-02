ad8e064f98ebbf249acfd227731fb0a51f97c90d
Sofia <sf.hazami@gmail.com>
2026-07-02 16:08:33 +0200

    Update pour fixe SonarQube Issues

--- src/__tests__/e2e/task.e2e.test.ts
+++ src/__tests__/e2e/task.e2e.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeAll, beforeEach, afterAll } from "vitest";
+import { describe, it, expect, beforeEach, afterAll } from "vitest";
 import { vi } from "vitest";
 import testPrisma from "./setup.js";
 
